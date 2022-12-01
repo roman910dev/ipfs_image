07b6b28d103fc36065a2f470ed6c71fd0ff1ded1
roman910dev <roman910dev@gmail.com>
2022-12-01 02:13:17 +0100

    Initial commit

---
+++ lib/ipfs_image.dart
@@ -0,0 +1,39 @@
+library ipfs_image;
+
+import 'package:flutter/widgets.dart';
+
+// ignore: camel_case_extensions
+extension ImageIPFS on Image {
+  static Image ipfs(String src) {
+    final Uri uri = Uri.parse(src);
+    final String cid = uri.host;
+    final String path = uri.path;
+
+    final List<String> providers = [
+      'https://$cid.ipfs.nftstorage.link$path',
+      'https://$cid.ipfs.dweb.link$path',
+      'https://$cid.ipfs.cf-ipfs.com$path',
+      'https://ipfs.io/ipfs/$cid$path',
+    ];
+
+    Image fromProvider(int index) {
+      return Image.network(
+          providers[index],
+          errorBuilder: index < providers.length - 1
+              ? (_, __, ___) => fromProvider(index+1)
+              : null
+      );
+    }
+
+    if (uri.scheme != 'ipfs') {
+      return Image.network(src);
+    } else {
+      return fromProvider(0);
+    }
+  }
+}
+
+extension NetworkImageIPFS on ImageProvider {
+  bool isIPFS() => this is NetworkImage &&
+      Uri.tryParse((this as NetworkImage).url)?.scheme == 'ipfs';
+}
